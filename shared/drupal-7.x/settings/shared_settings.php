c4fe4cabb2017c7ba93d113f24bb6d20055b97ea
btopro <bto108@psu.edu>
2015-03-03 20:59:31 -0500

    fast 404 not ready for prime time w.o. live config; file cache dumped too

--- shared/drupal-7.x/settings/shared_settings.php
+++ shared/drupal-7.x/settings/shared_settings.php
@@ -26,24 +26,6 @@ $conf['cache_class_cache_views'] = 'DrupalAPCCache';
 $conf['cache_class_cache_path_breadcrumbs'] = 'DrupalAPCCache';
 $conf['cache_class_cache_path'] = 'DrupalAPCCache';
 */
-# Filecache for ones that are big and don't change much
-# Comment this back in for filecache support, not all systems support this
-/*
-$conf['cache_backends'][] = 'sites/all/modules/ulmus/filecache/filecache.inc';
-# use this for file systems that don't have shared memory
-#$conf['filecache_directory'] = '/var/www/elmsln/config/private_files/_filecache/' . $conf['cache_prefix'];
-# hit file system in the shared memory portion for faster access
-#$conf['filecache_directory'] = '/dev/shm/drupal_filecache/' . $conf['cache_prefix'];
-#$conf['cache_default_class'] = 'DrupalFileCache';
-$conf['cache_class_cache_entity_user'] = 'DrupalFileCache';
-$conf['cache_class_cache_advagg_aggregates'] = 'DrupalFileCache';
-$conf['cache_class_cache_advagg_info'] = 'DrupalFileCache';
-$conf['cache_class_cache_update'] = 'DrupalFileCache';
-$conf['cache_class_cache_views_data'] = 'DrupalFileCache';
-$conf['cache_class_cache_page'] = 'DrupalFileCache';
-$conf['cache_class_cache_display_cache'] = 'DrupalFileCache';
-$conf['cache_class_cache_entity_node'] = 'DrupalFileCache';
-*/
 # Default DB for the ones that change too frequently and are small
 $conf['cache_default_class']    = 'DrupalDatabaseCache';
 # THIS MUST BE SERVED FROM DB FOR STABILITY
@@ -60,4 +42,4 @@ $databases['default']['default']['init_commands'] = array(
 
 // fast 404 to make advagg happy in the event fast 404 is default
 // we may do this in the future, right now just make sure the setting is correct
-$conf['404_fast_paths_exclude'] = '/\/(?:styles)\// to /\/(?:styles|advagg_(cs|j)s)\//';
+//$conf['404_fast_paths_exclude'] = '/\/(?:styles)\// to /\/(?:styles|advagg_(cs|j)s)\//';
