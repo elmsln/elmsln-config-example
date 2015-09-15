7addff1958c752cfde07697fe2e645c8d8e7c364
btopro <bto108@psu.edu>
2015-09-15 12:54:00 -0400

    updated shared settings based on current defaults, apdqc can be enabled by default too

--- shared/drupal-7.x/settings/shared_settings.php
+++ shared/drupal-7.x/settings/shared_settings.php
@@ -18,17 +18,16 @@ $conf['authcache_p13n_frontcontroller_path'] = 'authcache.php';
 #$conf['environment_indicator_overwritten_color'] = '#42b96a';
 # APC cache backend
 # Comment this back in for apc super fast support, not all systems support this
-/*
+
 #$conf['apc_show_debug'] = TRUE;
 $conf['cache_backends'][] = 'sites/all/modules/ulmus/apdqc/apdqc.cache.inc';
-$conf['cache_backends'][] = 'sites/all/modules/ulmus/apc/drupal_apc_cache.inc';
+#$conf['cache_backends'][] = 'sites/all/modules/ulmus/apc/drupal_apc_cache.inc';
 $conf['cache_backends'][] = 'sites/all/modules/ulmus/authcache/authcache.cache.inc';
 $conf['cache_backends'][] = 'sites/all/modules/ulmus/authcache/modules/authcache_builtin/authcache_builtin.cache.inc';
-#APDQCache
-$conf['cache_default_class'] = 'APDQCache';
+
 $conf['session_inc'] = 'sites/all/modules/ulmus/apdqc/apdqc.session.inc';
 $conf['lock_inc'] = 'sites/all/modules/ulmus/apdqc/apdqc.lock.inc';
-
+/*
 # APC as default, so these can be commented out
 $conf['cache_class_cache'] = 'DrupalAPCCache';
 $conf['cache_class_cache_admin_menu'] = 'DrupalAPCCache';
@@ -47,10 +46,10 @@ $conf['cache_class_cache_path'] = 'DrupalAPCCache';
 $conf['cache_class_cache_book'] = 'DrupalAPCCache';
 */
 # Default DB for the ones that change too frequently and are small
-$conf['cache_default_class']    = 'DrupalDatabaseCache';
+$conf['cache_default_class']    = 'APDQCache';
 # THIS MUST BE SERVED FROM DB FOR STABILITY
-$conf['cache_class_cache_cis_connector'] = 'DrupalDatabaseCache';
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
+$conf['cache_class_cache_cis_connector'] = 'APDQCache';
+$conf['cache_class_cache_form'] = 'APDQCache';
 
 // this is assuming all databases using this file operate off of default
 // this should always be true of ELMSLN connected systems but just be aware
